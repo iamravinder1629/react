a16bfce2d8929341b64a350db64906ecbb09ae5d
iamravinder1629 <iamravinder1629@gmail.com>
2024-01-22 23:44:43 +0530

    UI updated now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Utkarsha bansal
-        </a>
-      </header>
+    <div>
+      <h1>Ravinder singh</h1>
+      <h1>This is my first deployed reactjs project on git hub</h1>
+      <h3>bye bye</h3>
     </div>
   );
 }
