73543ac660be84a86058f449e135eb3881fcb86a
iamravinder1629 <iamravinder1629@gmail.com>
2024-01-22 23:30:22 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ravinder singh
+          Utkarsha bansal
         </a>
       </header>
     </div>
